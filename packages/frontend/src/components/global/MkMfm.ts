81841d9ff3fa95d42d0c71e940b6a2c6857b66b6
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-11-10 23:26:51 +0900

    feat(frontend): saize menu (#109)

--- packages/frontend/src/components/global/MkMfm.ts
+++ packages/frontend/src/components/global/MkMfm.ts
@@ -17,6 +17,7 @@ import MkCode from '@/components/MkCode.vue';
 import MkCodeInline from '@/components/MkCodeInline.vue';
 import MkGoogle from '@/components/MkGoogle.vue';
 import MkSparkle from '@/components/MkSparkle.vue';
+import HanaSaizeMenuBadge from '@/components/HanaSaizeMenuBadge.vue';
 import MkA, { type MkABehavior } from '@/components/global/MkA.vue';
 import TmsFormula from '@/components/TmsFormula.vue';
 import { defaultStore } from '@/store.js';
@@ -338,6 +339,15 @@ export default function (props: MfmProps, { emit }: { emit: SetupContext<MfmEven
 							const clickEv = typeof token.props.args.ev === 'string' ? token.props.args.ev : '';
 							emit('clickEv', clickEv);
 						} }, genEl(token.children, { scale, disableNyaize, disableRjNumber: true }));
+					}
+					case 'saize': {
+						if (token.children.length === 1 && token.children[0].type === 'text') {
+							return h(HanaSaizeMenuBadge, {
+								menuCode: token.children[0].props.text,
+							});
+						} else {
+							return genEl(token.children, scale);
+						}
 					}
 				}
 				if (style === undefined) {
